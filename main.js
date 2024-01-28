9990f27128aa7cbad472a25c986972e9262be83e
Serhii-Kor <134406132+Serhii-Kor@users.noreply.github.com>
2024-01-28 21:10:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+const PLAYFIELD_COLUMNS = 10;
+const PLAYFIELD_ROWS=20;
+
+
+const TETROMINO_NAMES = [
+    'O'
+];
+
+const TETROMINOES = {
+    'O':[
+        [1,1],
+        [1,1]
+    ]
+};
+
+let playfield;
+let tetromino;
+
+function convertPositionToIndex(row, column){
+    return row * PLAYFIELD_COLUMNS + column;
+}
+
+function generatePlayfield(){
+    for(let i=0; i<PLAYFIELD_ROWS*PLAYFIELD_COLUMNS; i++){
+        const div = document.createElement('div');
+        document.querySelector('.tetris').append(div);
+
+    }
+
+    playfield = new Array(PLAYFIELD_ROWS).fill()
+                    .map(()=> new Array(PLAYFIELD_COLUMNS).fill(0))
+
+    console.log(playfield);
+}
+
+function generateTetromino(){
+    const nameTetro = 'O';
+    const matrixTetro = TETROMINOES[nameTetro];
+
+    const columnTetro = 5;
+    const rowTetro    = 3;
+
+    tetromino = {
+        name: nameTetro,
+        matrix: matrixTetro,
+        row: rowTetro,
+        column: columnTetro,
+    }
+}
+
+generatePlayfield();
+generateTetromino();
+const cells = document.querySelectorAll('.tetris div');
+
+function drawTetromino(){
+    const name = tetromino.name;
+    const tetrominoMatrixSize = tetromino.matrix.length;
+
+    for(let row = 0; row<tetrominoMatrixSize; row++){
+        for(let column = 0; column< tetrominoMatrixSize; column++){
+
+            const cellIndex = convertPositionToIndex(tetromino.
+                row+row,tetromino.column+column);
+                cells[cellIndex].classList.add(name);
+
+        }
+    }
+}
+
+drawTetromino();
